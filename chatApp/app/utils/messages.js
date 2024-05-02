a895911e4dc2265f2ddca46885933a499bfacae4
Tigran008 <66358286+Tigran008@users.noreply.github.com>
2024-05-03 02:49:25 +0400

    Delete chatApp directory

--- chatApp/app/utils/messages.js
+++
@@ -1,9 +0,0 @@
-const moment = require("moment");
-const formatMessages = (username, text) => {
-  return {
-    text: text,
-    username: username,
-    time: moment().format("h:mm a"),
-  };
-};
-module.exports = formatMessages;
